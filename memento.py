bc58baeef12b16a5bcebd224dd4f220516b23b89
Timm Hoffmann <timm.hoffmann@thimmojn.de>
2019-05-11 15:46:19 +0200

    add memento (game state)

---
+++ memento.py
@@ -0,0 +1,51 @@
+import enum, lxml.etree
+from board import Board
+from move import Move
+
+"""
+Game state.
+"""
+
+class Player(enum.Enum):
+    Red = 'RED'
+    Blue = 'BLUE'
+
+
+class Memento:
+    def __init__(self, turn, startPlayer, currentPlayer, playerNames, board, lastMove):
+        self.turn = turn
+        self.startPlayer = startPlayer
+        self.currentPlayer = currentPlayer
+        self.playerNames = playerNames
+        self.board = board
+        self.lastMove = lastMove
+
+    @staticmethod
+    def __getPlayerName(stateNode, player):
+        expr = '*[local-name() = $tag and @color = $color]/@displayName'
+        return next(iter(stateNode.xpath(expr, tag=player.value.lower(), color=player.value)))
+
+    @classmethod
+    def fromXML(cls, mementoNode):
+        # get first state node in memento
+        stateNode = mementoNode.find('state')
+
+        turn = int(stateNode.get('turn'))
+        startPlayer = Player(stateNode.get('startPlayer'))
+        currentPlayer = Player(stateNode.get('currentPlayer'))
+
+        playerNames = dict(
+            (p, Memento.__getPlayerName(stateNode, p))
+            for p in Player
+        )
+
+        board = Board.fromXML(stateNode.find('board'))
+
+        lastMoveNode = stateNode.find('lastMove')
+        lastMove = Move.fromXML(lastMoveNode) if lastMoveNode is not None else None
+
+        # TODO: condition???
+
+        return cls(turn, startPlayer, currentPlayer, playerNames, board, lastMove)
+
+# -*- encoding: utf-8-unix -*-
---
+++ memento_test.py
@@ -0,0 +1,32 @@
+import lxml.etree
+from memento import Memento, Player
+
+
+def test_memento_from_official_doc():
+    mementoXML = lxml.etree.XML("""<data class="memento">
+  <state class="state" turn="0" startPlayer="RED" currentPlayer="RED">
+    <red displayName="Unknown" color="RED" />
+    <blue displayName="Unknown" color="BLUE" />
+    <board>
+      <fields>
+        <field x="0" y="0" state="EMPTY" />
+        <field x="0" y="9" state="EMPTY" />
+      </fields>
+      <fields>
+        <field x="9" y="0" state="EMPTY" />
+        <field x="9" y="9" state="EMPTY" />
+      </fields>
+    </board>
+  </state>
+</data>""")
+    memento = Memento.fromXML(mementoXML)
+
+    assert memento.turn == 0
+    assert memento.startPlayer is Player.Red
+    assert memento.currentPlayer is Player.Red
+    assert memento.playerNames[Player.Red] == 'Unknown'
+    assert memento.playerNames[Player.Blue] == 'Unknown'
+    assert repr(memento.board) == '\n'.join(' '.join(10 * [' ']) for _ in range(10))
+    assert memento.lastMove is None
+
+# -*- encoding: utf-8-unix -*-
